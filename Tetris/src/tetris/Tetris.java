4e00fcaf3534906399e1d315efe6845314805138
PReynaud <pierre.reynaud@hotmail.fr>
2014-05-07 10:41:21 +0200

    Deuxieme

--- Tetris/src/tetris/Tetris.java
+++ Tetris/src/tetris/Tetris.java
@@ -1,16 +1,10 @@
 package tetris;
 
-/**
- *
- * @author p1105624
- */
 public class Tetris {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+
     }
     
 }
