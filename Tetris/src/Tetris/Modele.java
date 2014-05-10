2817c7bf5a7d1f2f77211fbdbbeeb3028c0e8ff9
Pierre <Pierre@PC-de-Pierre.home>
2014-05-10 17:11:52 +0200

    Changement de nom, ajout de Obserrver et Observable

--- Tetris/src/Tetris/Modele.java
+++ Tetris/src/Tetris/Modele.java
@@ -4,6 +4,11 @@ import java.util.Observable;
 
 public class Modele extends Observable{
     private Grille _grille;
+    private Joueur _joueur;
     
     
+    public void ajout_piece_grille(Piece une_piece, int x, int y){
+        une_piece.setX(x);
+        une_piece.setY(y);
+    }
 }
